edb0bdaa21e171826f103b9e6cc4e595aa92c5c4
Mindaugas <mindaugasgrybauskas@protonmail.com>
2023-06-01 15:44:03 +0200

    Set up blank files for js, html ,css link them. Set up webpack, loaders, linters and ignore files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+const container = document.getElementById('container');
+const newTitle = document.createElement('h1');
+newTitle.innerHTML = 'hello world';
+container.appendChild(newTitle);
